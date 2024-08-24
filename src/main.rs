7f174dc385869e6e86fe23edae23fb619d7bdbfc
Valik Solorzano <vsolorzanobarboza@zebra.com>
2024-08-24 19:41:26 -0400

    initial version

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::{env, process};
+
+use spellbook_minimization::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = spellbook_minimization::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
